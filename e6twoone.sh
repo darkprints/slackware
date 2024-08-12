8ab044ba17d016a449f1c493d127785e3ce91114
darkprints <38918906+darkprints@users.noreply.github.com>
2024-08-12 17:23:46 -0500

    Update e6twoone.sh

--- e6twoone.sh
+++ e6twoone.sh
@@ -1,2 +1,4 @@
 wget -c "https://static1.e621.net/data/d3/0a/d30a52b872c3f1a3073b91538a721898.jpg" -O "/root/Pictures/image1.jpg"
 wget -c "https://static1.e621.net/data/19/b0/19b025a6e613de629e83915ce55e9a04.jpg" -O "/root/Pictures/image2.jpg"
+
+# wget -O - http://dl.dropbox.com/u/11210438/flockonus-stack.sh | bash
