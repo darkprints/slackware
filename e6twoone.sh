4c8334f1291320d6e6a9034d153096100c585765
darkprints <38918906+darkprints@users.noreply.github.com>
2024-08-12 17:09:45 -0500

    Create e6twoone.sh

---
+++ e6twoone.sh
@@ -0,0 +1,2 @@
+wget "https://static1.e621.net/data/d3/0a/d30a52b872c3f1a3073b91538a721898.jpg" -P "/root/Pictures/image1.jpg"
+wget "https://static1.e621.net/data/19/b0/19b025a6e613de629e83915ce55e9a04.jpg" -P "/root/Pictures/image2.jpg"
