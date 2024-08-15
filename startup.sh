58c9df7723546f18b542841ca640a89a088362d2
darkprints <38918906+darkprints@users.noreply.github.com>
2024-08-14 20:00:01 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 wget "https://github.com/darkprints/slackware/raw/master/slackware.sh" -P "/opt/1.startup/"
-chmod -x slackware.sh
-xfce4-terminal --geometry=80x43+1000+50 --title=start up "/opt/1.startup/slackware.sh"
+chmod +x "/opt/1.startup/slackware.sh"
+clear
+echo "GAMES"
+xfce4-terminal --geometry=80x43+1000+50 --title="slackware" -e "/opt/1.startup/slackware.sh"
