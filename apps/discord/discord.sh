a9852a728bf28270d80aa3308ca2df249d8f181b
darkprints <38918906+darkprints@users.noreply.github.com>
2024-08-29 21:27:06 -0500

    Update discord.sh

--- apps/discord/discord.sh
+++ apps/discord/discord.sh
@@ -15,17 +15,15 @@ tar -xvzf "/opt/apps_backup/discord.tar.gz" -C "/opt/apps_backup/"
 cp "/opt/apps_backup/discord-0.0.66.deb" "/opt/apps_backup/discord"
 /opt/apps_backup/discord/discord.SlackBuild
 
-telegram-5telegram-5.4.1-x86_64-1_SBo.tgz.4.1-x86_64-1_SBo.tgz
-
 rm -r "/opt/apps_backup/discord"
-rm "/opt/apps_backup/discordtar.gz"
-rm "/opt/apps_backup/discord-0.0.65.xz.aa"
-rm "/opt/apps_backup/discord-0.0.65.xz.ab"
-rm "/opt/apps_backup/discord-0.0.65.xz.ac"
-rm "/opt/apps_backup/discord-0.0.65.xz.ad"
-rm "/opt/apps_backup/discord-0.0.65.xz.ae"
-rm "/opt/apps_backup/discord-0.0.65.xz.af"
+rm "/opt/apps_backup/discord.tar.gz"
+rm "/opt/apps_backup/discord-0.0.66.xz.aa"
+rm "/opt/apps_backup/discord-0.0.66.xz.ab"
+rm "/opt/apps_backup/discord-0.0.66.xz.ac"
+rm "/opt/apps_backup/discord-0.0.66.xz.ad"
+rm "/opt/apps_backup/discord-0.0.66.xz.ae"
+rm "/opt/apps_backup/discord-0.0.66.xz.af"
 
 
-mv "/tmp/discord-0.0.65-x86_64-1_SBo.tgz" "/opt/apps_backup/discord-0.0.65-x86_64-1_SBo.tgz"
-upgradepkg --install-new "/opt/apps_backup/discord-0.0.65-x86_64-1_SBo.tgz"
+mv "/tmp/discord-0.0.66-x86_64-1_SBo.tgz" "/opt/apps_backup/discord-0.0.66-x86_64-1_SBo.tgz"
+upgradepkg --install-new "/opt/apps_backup/discord-0.0.66-x86_64-1_SBo.tgz"
