00fe51ac80de4d8d8cefe4c895064134a741c0a4
darkprints <38918906+darkprints@users.noreply.github.com>
2024-08-29 21:12:29 -0500

    Update discord.sh

--- apps/discord/discord.sh
+++ apps/discord/discord.sh
@@ -1,18 +1,18 @@
 # Installing telegram
 
 
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.aa" -P "/opt/apps_backup/"
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.ab" -P "/opt/apps_backup/"
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.ac" -P "/opt/apps_backup/"
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.ad" -P "/opt/apps_backup/"
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.ae" -P "/opt/apps_backup/"
-wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.65.deb.af" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.aa" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.ab" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.ac" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.ad" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.ae" -P "/opt/apps_backup/"
+wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord-0.0.66.deb.af" -P "/opt/apps_backup/"
 wget "https://github.com/darkprints/slackware/raw/master/apps/discord/discord.tar.gz" -P "/opt/apps_backup/"
 
-cat /opt/apps_backup/discord-0.0.65.deb.* | tar xzvf - -C "/opt/apps_backup/"
+cat /opt/apps_backup/discord-0.0.66.deb.* | tar xzvf - -C "/opt/apps_backup/"
 
 tar -xvzf "/opt/apps_backup/discord.tar.gz" -C "/opt/apps_backup/"
-cp "/opt/apps_backup/discord-0.0.65.deb" "/opt/apps_backup/discord"
+cp "/opt/apps_backup/discord-0.0.66.deb" "/opt/apps_backup/discord"
 /opt/apps_backup/discord/discord.SlackBuild
 
 telegram-5telegram-5.4.1-x86_64-1_SBo.tgz.4.1-x86_64-1_SBo.tgz
