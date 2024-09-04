9462a89c69446842b64a584d212392d045d6b419
darkprints <38918906+darkprints@users.noreply.github.com>
2024-09-04 11:04:07 -0500

    Create virt-manager.sh

---
+++ apps/virt-manager/virt-manager.sh
@@ -0,0 +1,22 @@
+# Installing Virt-Manager
+
+        spice-protocol
+    spice
+spice-gtk
+gtk-vnc
+        yajl
+    libvirt
+libvirt-glib
+libvirt-python
+        osinfo-db-tools
+    osinfo-db    
+libosinfo
+
+
+
+
+
+
+
+
+
